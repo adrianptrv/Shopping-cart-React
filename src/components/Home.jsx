2fa95d941b5397d08c60327a022c7675be920283
adrianptrv <adrian.prtv1@gmail.com>
2024-07-04 14:43:27 +0300

    Fixed homepage image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,12 @@ import { Link } from 'react-router-dom';
 function Home() {
 
   return (
-    <>
       <div className='homeBack'>
         <h1>
           Welcome to our PC hardware store!
         </h1>
         <Link to="/Shopping-cart-React/shop"><button className='homeShop'>Shop now</button></Link>
       </div>
-    </>
   )
 }
 
